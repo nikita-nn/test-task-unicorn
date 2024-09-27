714e580f684bb0d93a0017dcac3316e7146b03be
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-27 16:36:40 +0300

    ref: all sections in single files

---
+++ src/components/sections/MainPage/Footer.tsx
@@ -0,0 +1,3 @@
+export const MainPageFooter = () => (
+  <footer className={"footer-main-page"}></footer>
+);
---
+++ src/components/sections/MainPage/MainSection.tsx
@@ -0,0 +1,37 @@
+import { Planet } from "../../Planet.tsx";
+import { mockStats } from "../../../data/stats.ts";
+
+export const MainSection = () => (
+  <section className="main-section">
+    <div className={"flex flex-col gap-10"}>
+      <h1 className={"main-heading"}>
+        <span>Explore Your own planet</span>
+        <br />
+        <span>In</span>
+        <span className={"text-transparent text-stroke"}> our New </span>
+        <span>metaverse</span>
+      </h1>
+      <Planet />
+      <p className="main-description font-avenir">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+    <div className={"flex flex-col gap-4 text-center"}>
+      <h1 className={"main-subheading"}>roadmap stats</h1>
+      <div className={"main-stats-container"}>
+        {mockStats.map((stat, index) => (
+          <>
+            <div className={`main-stats-elem`} key={index}>
+              <h1 className={"main-stats-heading"}>{stat.data}</h1>
+              <p className={"main-stats-description"}>{stat.name}</p>
+            </div>
+            <div className={"w-[12rem] h-[1px] bg-[#D2C4C4]"} />
+          </>
+        ))}
+      </div>
+    </div>
+  </section>
+);
