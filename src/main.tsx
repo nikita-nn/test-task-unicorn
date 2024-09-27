9b87484503e00f29a25fddea0d04b863dc3a059b
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-27 16:18:39 +0300

    delete: userProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/globals.css";
 import "./styles/fonts.css";
-import { UserProvider } from "./providers/UserProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </StrictMode>,
 );
