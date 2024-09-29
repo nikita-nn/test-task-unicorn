e63f575cd8af82eb8c2779b9654081f07f034946
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-29 17:00:31 +0300

    feat: correct working with docs and correct line height

--- src/components/sections/MainPage/BetaTestSection.tsx
+++ src/components/sections/MainPage/BetaTestSection.tsx
@@ -14,7 +14,7 @@ export const BetaTestSection = () => {
     <section className="beta-test-section">
       <div className="flex flex-col gap-4">
         <h1 className={"beta-test-heading"}>Beta test registration</h1>
-        <p className={"beta-test-description  font-avenir"}>
+        <p className={"beta-test-description font-avenir"}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
