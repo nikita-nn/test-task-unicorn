adf6e4c1953a5ce1ea7a42e814055c48b799665f
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-29 19:19:41 +0300

    feat: minified chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+    }),
+  ],
+  build: {
+    minify: true,
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/[hash].js",
+        manualChunks: {
+          react: ["react"],
+          reactDom: ["react-dom"],
+          viem: ["viem"],
+          wagmi: ["wagmi"],
+          wagmiCore: ["@wagmi/core"],
+          remixRunRouter: ["@remix-run/router"],
+          reactRouter: ["react-router"],
+          zustand: ["zustand"],
+        },
+      },
+    },
+  },
 });
