86af4a2e39294e9f48e66f0ef54d9e3295f48202
Nikita <146779463+nikita-nn@users.noreply.github.com>
2024-09-29 21:11:50 +0300

    doc: documented useAnimate hook

--- src/hooks/animateMe.ts
+++ src/hooks/animateMe.ts
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Хук для создания базовой анимации
+ * @params baseClass - параметр показывающий базовый стиль обьекта, к которому применяется анимация
+ */
+
 export const useAnimate = (baseClass: string) => {
   const animatedClass = baseClass + " transition-all duration-300 ease-in-out";
   const [animationClass, setAnimationClass] = useState<string>(animatedClass);
